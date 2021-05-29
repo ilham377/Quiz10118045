14005e3def0f05a2a06b87a035e0258cf8a738f9
Ilham377 <ilham377@gmail.com>
2021-05-29 10:52:45 +0700

    komen

--- app/src/main/java/com/iapps/quiz10118045/MainActivity.java
+++ app/src/main/java/com/iapps/quiz10118045/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//TGL : 29/05/2021
+//NIM : 10118045
+//Nama : Muhammad Ilham Apriyadi
+//Kelas : IF2
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
